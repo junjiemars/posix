6cf780f13dc8d58b6df35625482b06e7a763d66f
findidiot <junjie.mars@icloud.com>
2025-02-24 11:15:45 +0800

    init

---
+++ nasrt.h
@@ -0,0 +1,17 @@
+#ifndef _NASRT_H_
+#define _NASRT_H_
+
+#include <npstd.h>
+#include <assert.h>
+
+
+#if !(NM_HAVE_STATIC_ASSERT)
+#  ifdef static_assert
+#    undef static_assert
+#  endif
+#  define static_assert(e, m) enum {static_assert = 1/!!((e) && (m))}
+#endif  /* NM_HAVE_STATIC_ASSERT */
+
+
+
+#endif /* end of _NASRT_H_ */
---
+++ nint.h
@@ -0,0 +1,13 @@
+/**
+ * Nore std int
+ */
+#ifndef _NINT_H_
+#define _NINT_H_
+
+#include <npstd.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+
+
+#endif /* end of _NINT_H_ */
